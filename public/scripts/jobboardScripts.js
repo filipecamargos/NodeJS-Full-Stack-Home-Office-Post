127fa7ae28c7127156e6cea57ac5b02047e94273
filipecamargos <filipe.atrabalho@gmail.com>
2020-06-16 11:00:55 -0600

    Small Css on the footer!

--- public/scripts/jobboardScripts.js
+++ public/scripts/jobboardScripts.js
@@ -4,9 +4,9 @@ $(function() {
         // Get the Information
         var title = $(this).find('h3.title').text();
         var company_name = $(this).find('h3.company_name').text();
-        var candidate_required_location = $(this).find('h4.candidate_required_location').text();
+        var candidate_required_location = $(this).find('span.candidate_required_location').text();
+        var job_type = $(this).find('span.job_type').text();
         var description = $(this).find('div.description').text();
-
 
 
         //Set the Information
@@ -14,6 +14,7 @@ $(function() {
         $("#jobCompany").html(company_name);
         $("#jobLocation").html(candidate_required_location);
         $("#jobDescription").html(description);
+        $("#JobType").html(job_type);
 
     })
 })
